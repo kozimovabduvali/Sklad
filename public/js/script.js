ad00316cd689b00178a1bfbf6014f5dd534b8391
kozimovabduvali <kozimovabduvali2006@gmail.com>
2025-09-29 00:07:20 +0500

    header done

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+// Home page 
+
+// Hero slider
+
+let swiper = new Swiper(".sklad-hero__swiper", {
+  spaceBetween: 15,
+
+  navigation: {
+    nextEl: ".swiper-button-next__main__hero",
+    prevEl: ".swiper-button-prev__main__hero",
+  },
+});
